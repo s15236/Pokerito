287a5ef1525e28fba6fa4332098bec5693fedc60
s15236 <s15236@pjwstk.edu.pl>
2023-02-24 17:22:20 +0100

    Added function for printing random cards. Did some testing.

--- Pokerito.java
+++ Pokerito.java
@@ -4,10 +4,123 @@ public class Pokerito {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        for (int i = 0; i < 5; i++) {
+            System.out.println(randomCard());
+        }
+
         int yourMatches = 0;
         int computerMatches = 0;
 
         scan.close();
+    }
+
+    /**
+     * Function name – randomCard
+     *
+     * @return (String)
+     * <p>
+     * Inside the function:
+     * 1. Gets a random number between 1 and 13.
+     * 2. Returns a card that matches the random number (get the String values from cards.text).
+     */
+    public static String randomCard() {
+        double number = (Math.random() * 13) + 1;
+        String card = "";
+        switch ((int) number) {
+            case 1:
+                return "   _____\n" +
+                        "  |A _  |\n" +
+                        "  | ( ) |\n" +
+                        "  |(_'_)|\n" +
+                        "  |  |  |\n" +
+                        "  |____V|\n";
+            case 2:
+                return "   _____\n" +
+                        "  |2    |\n" +
+                        "  |  o  |\n" +
+                        "  |     |\n" +
+                        "  |  o  |\n" +
+                        "  |____Z|\n";
+            case 3:
+                return "   _____\n" +
+                        "  |3    |\n" +
+                        "  | o o |\n" +
+                        "  |     |\n" +
+                        "  |  o  |\n" +
+                        "  |____E|\n";
+            case 4:
+                return "   _____\n" +
+                        "  |4    |\n" +
+                        "  | o o |\n" +
+                        "  |     |\n" +
+                        "  | o o |\n" +
+                        "  |____h|\n";
+            case 5:
+                return "   _____ \n" +
+                        "  |5    |\n" +
+                        "  | o o |\n" +
+                        "  |  o  |\n" +
+                        "  | o o |\n" +
+                        "  |____S|\n";
+            case 6:
+                return "   _____ \n" +
+                        "  |6    |\n" +
+                        "  | o o |\n" +
+                        "  | o o |\n" +
+                        "  | o o |\n" +
+                        "  |____6|\n";
+            case 7:
+                return "   _____ \n" +
+                        "  |7    |\n" +
+                        "  | o o |\n" +
+                        "  |o o o|\n" +
+                        "  | o o |\n" +
+                        "  |____7|\n";
+            case 8:
+                return "   _____ \n" +
+                        "  |8    |\n" +
+                        "  |o o o|\n" +
+                        "  | o o |\n" +
+                        "  |o o o|\n" +
+                        "  |____8|\n";
+            case 9:
+                return "   _____ \n" +
+                        "  |9    |\n" +
+                        "  |o o o|\n" +
+                        "  |o o o|\n" +
+                        "  |o o o|\n" +
+                        "  |____9|\n";
+            case 10:
+                return "   _____ \n" +
+                        "  |10  o|\n" +
+                        "  |o o o|\n" +
+                        "  |o o o|\n" +
+                        "  |o o o|\n" +
+                        "  |___10|\n";
+            case 11:
+                return "   _____\n" +
+                        "  |J  ww|\n" +
+                        "  | o {)|\n" +
+                        "  |o o% |\n" +
+                        "  | | % |\n" +
+                        "  |__%%[|\n";
+            case 12:
+                return "   _____\n" +
+                        "  |Q  ww|\n" +
+                        "  | o {(|\n" +
+                        "  |o o%%|\n" +
+                        "  | |%%%|\n" +
+                        "  |_%%%O|\n";
+            case 13:
+                return "   _____\n" +
+                        "  |K  WW|\n" +
+                        "  | o {)|\n" +
+                        "  |o o%%|\n" +
+                        "  | |%%%|\n" +
+                        "  |_%%%>|\n";
+            default:
+                return "ERROR";
+        }
     }
 
 }
