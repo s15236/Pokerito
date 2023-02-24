6653051f2e17f41def73e831b4ba4680002da5ac
s15236 <s15236@pjwstk.edu.pl>
2023-02-24 15:10:03 +0100

    Initial commit.

---
+++ Pokerito.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Pokerito {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int yourMatches = 0;
+        int computerMatches = 0;
+
+        scan.close();
+    }
+
+}
